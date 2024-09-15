9b00f9c7b9b425a0b796ac90509469b51ecff42d
Victor Assis <victor.assis.alves@gmail.com>
2024-09-15 15:06:21 -0400

    first

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
-    # server = socket.create_server(("localhost", 9092), reuse_port=True)
-    # server.accept() # wait for client
+
+    server = socket.create_server(("localhost", 9092), reuse_port=True)
+    server.accept()  # wait for client
 
 
 if __name__ == "__main__":
